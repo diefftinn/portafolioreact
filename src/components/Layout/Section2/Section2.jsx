2bc3e9912a309edcd66bebed3e575d1fd9271e93
diefftin <dieferti74@gmail.com>
2024-10-31 02:46:37 -0500

    b portafolioreact

--- src/components/Layout/Section2/Section2.jsx
+++ src/components/Layout/Section2/Section2.jsx
@@ -4,17 +4,33 @@ import logojs from'../../../assets/images/logojs.png'
 import '../Section2/section2.css'
 
 
-export const Section2 =  () =>{
-    return(
+
+export const Section2 = () => {
+    return (
         <div className="container-section2">
-       <ImgComponent src= {logojs} alt={logojs}/>
-    <p>
-     Uno de los esenarios como la programacion permite conecer varias facetas 
-     
-    </p>
-    <button>generar</button>
-</div>
+            <h2>Actividades de preferencia</h2>
+            <div className="content-section">
+                <div className="text-section">
+                    <div className="study-item">
+                        <h3>Lectura</h3>
+                        <p>Intento practicar  la lectura porque es una forma de conocer nuevas ideas y perspectivas, ya que esta me ayuda a expandir el conocimiento sobre diferentes temas y mejora la concentración y comprensión, enriqueciendo la imaginación y creatividad.</p>
+                    </div>
+                    <div className="study-item">
+                        <h3>Programación</h3>
+                        <p>Practico la programación porque me gusta crear soluciones para problemas reales, mejorar las habilidades tecnológicas, conociendo nuevas oportunidades para el futuro.</p>
+                    </div>
+                    <div className="study-item">
+                        <h3>Deporte</h3>
+                        <p>El deporte es un insentivo para mi ya que gracias al el he mejorado mi percepcion de que es mantener una buena salud  como mantenerse activo y saludable,  en el cual este ayuda a mejorar la resistencia y fuerza, y te da un tiempo para relajarte y liberar el estrés. Además, es una excelente manera de desarrollar disciplina y enfoque.
+                        </p>
+                    </div>
+                </div>
+                <div className="image-section">
+                    <img src={logojs} alt="Estudios" />
+                </div>
+            </div>
+        </div>
+    );
+};
 
 
-    )
-}
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Header } from '../../Layout/Header/Header'
-import { Section } from '../../Layout/Section/section'
+import { Section}from '../../Layout/Section/Section'
+
+
 import { IntroProfile } from '../../Layout/IntroProfile/IntroProfile'
 import { Section2 } from '../../Layout/Section2/Section2'
 
